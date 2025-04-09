47274289c5adc4df5d2bf5f8fd49eeec6491d08b
Garret47 <abahmetov10@gmail.com>
2025-04-09 23:25:47 +0600

    Add systemd timer and extend script with monitoring
    - Implement systemd timer for per-minute service execution
    - Enhance script with process monitoring and log output

--- Makefile
+++ Makefile
@@ -2,14 +2,17 @@ NAME=monitor_process
 BIN_DIR = /usr/local/bin
 SERVICE_DIR = /etc/systemd/system
 CONF_DIR = /etc/$(NAME)
-SERVICE_FILE = $(NAME).service
-SCRIPT_FILE = $(NAME).sh
-CONF_FILE = $(NAME).conf
+
+SERVICE_FILES = $(wildcard *.service)
+SCRIPT_FILES = $(wildcard *.sh)
+CONF_FILES = $(wildcard *.conf)
+TIMER_FILES = $(wildcard *.timer)
+
 
 all: start
 
 install: create_directories copy_files
-
+clear: delete_scripts stop
 
 create_directories:	
 	@echo "Checking directories..."
@@ -22,24 +25,33 @@ create_directories:
 
 copy_files:
 	@echo "Copying files..."
-	cp $(SCRIPT_FILE) $(BIN_DIR)
-	cp $(SERVICE_FILE) $(SERVICE_DIR)
-	cp $(CONF_FILE) $(CONF_DIR)
+	cp $(SCRIPT_FILES) $(BIN_DIR)
+	cp $(SERVICE_FILES) $(SERVICE_DIR)
+	cp $(TIMER_FILES) $(SERVICE_DIR)
+	cp $(CONF_FILES) $(CONF_DIR)
 
 start:
 	echo "Enabling and starting the systemd service..."
 	systemctl daemon-reload
-	systemctl enable $(SERVICE_FILE)
-	systemctl start $(SERVICE_FILE)
+	$(foreach timer,$(TIMER_FILES), \
+		systemctl enable $(timer) && systemctl start $(timer) \
+	) 
 
-clear:
-	@echo "Cleaning up files and services..."
-	rm -f $(BIN_DIR)/$(SCRIPT_FILE)
-	rm -f $(SERVICE_DIR)/$(SERVICE_FILE)
-	rm -f $(CONF_DIR)/$(CONF_FILE)
-	systemctl stop $(SERVICE_FILE)
-	systemctl disable $(SERVICE_FILE)
+stop:
+	@echo "Stop services"
+	$(foreach timer, $(TIMER_FILES), \
+		systemctl stop $(timer) && systemctl disable $(timer) \
+	)
 	systemctl daemon-reload
 
-.PHONY: all install create_directories copy_files start clear
+
+delete_scripts:
+	@echo "Delete scripts"
+	$(foreach script,$(SCRIPT_FILES), rm -f $(BIN_DIR)/$(script))
+	$(foreach service,$(SERVICE_FILES), rm -f $(SERVICE_DIR)/$(service))
+	$(foreach conf,$(CONF_FILES), rm -f $(CONF_DIR)/$(conf))
+	$(foreach timer, $(TIMER_FILES), rm -f $(SERVICE_DIR)/$(timer))
+
+
+.PHONY: all install create_directories copy_files start stop clear delete_scripts
 
--- monitor_process.sh
+++ monitor_process.sh
@@ -1,18 +1,64 @@
 #!/bin/bash
 
+MSG_DEBUG="[DEBUG]"
 MSG_INFO="[INFO]"
 MSG_ERROR="[ERROR]"
 MSG_CRITICAL="[CRITICAL]"
 LOG_FILE="${LOG_FILE:-/var/log/monitoring.log}"
+PID_FILE="$RUNTIME_DIRECTORY/monitoring.pid"
 
 log_message() {
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> "$LOG_FILE"
 }
 
-log_message "$MSG_INFO Monitoring process ($PROCESS_NAME) started"
-CURRENT_PID=$(pgrep -x "$PROCESS_NAME")
-CODE=$?
-if [[ $CODE -eq 0 ]]; then
-    CURRENT_PID=$(echo "$CURRENT_PID" | head -n1)
+debug_log(){
+    if [[ $DEBUG ]]; then
+        log_message "$MSG_DEBUG $1"
+    fi
+}
 
+debug_log "Monitoring process ($PROCESS_NAME) started"
+debug_log "$RUNTIME_DIRECTORY"
+
+CURRENT_PIDS=$(pgrep -x "$PROCESS_NAME")
+CODE=$?
+
+if [[ $CODE -eq 0 ]]; then
+    # The process may have multiple PIDs, so we will iterate through each one
+    OLD_PROCESS_NAME=$(head -n 1 $PID_FILE) || OLD_PROCESS_NAME=""
+    if [[ -f $PID_FILE && $OLD_PROCESS_NAME == $PROCESS_NAME ]]; then
+        OLD_PIDS=$(tail -n +2 $PID_FILE)
+        MAX_RESTARTS=$(echo "$OLD_PIDS" | wc -w)
+        CURRENT_COUNT=$(echo "$CURRENT_PIDS" | wc -w)
+        RESTART_COUNT=0
+        MISSING_PIDS=()
+        # Iterate through the old PIDs and check if they are present in the current list
+        for old_pid in $OLD_PIDS; do
+            if ! echo "$CURRENT_PIDS" | grep -q "$old_pid"; then
+                MISSING_PIDS+=("$old_pid")
+            fi
+        done
+        MISSING_COUNT=${#MISSING_PIDS[@]}
+        
+        debug_log "Missing PIDs: [${MISSING_PIDS[*]}] | Total missing: $MISSING_COUNT"
+        if [[ $CURRENT_COUNT -lt $MAX_RESTARTS ]]; then
+            # Calculate how many processes were simply stopped
+            STOPPED_COUNT=$((MAX_RESTARTS - CURRENT_COUNT))
+            # Remove the last elements (processes that were simply stopped)
+            MISSING_PIDS=("${MISSING_PIDS[@]::$MISSING_COUNT-$STOPPED_COUNT}")
+            debug_log "Stopped PIDs count: $STOPPED_COUNT | Missing PIDs: [${MISSING_PIDS[*]}]"
+        fi
+        
+        for pid in "${MISSING_PIDS[@]}"; do
+            RESTART_COUNT=$((RESTART_COUNT + 1))
+            log_message "$MSG_INFO Process $PROCESS_NAME with PID $pid was restarted"
+        done
+
+        debug_log "Total restarts detected: $RESTART_COUNT out of $MAX_RESTARTS possible"
+    fi
+    echo "$PROCESS_NAME" > $PID_FILE
+    echo "$CURRENT_PIDS" >> $PID_FILE
+
+else
+    debug_log "Process $PROCESS_NAME is not running"
 fi
